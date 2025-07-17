4e174c1e39cc2c49a7a90e0b9ecf6eb73efb7209
Willman <willmansatria5@gmail.com>
2025-07-17 21:11:09 +0700

    add secret hardcoded dan dependensi rentan

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function add(a,b){
     return a + b;
 }
-module.exports = add;
+
+function subtract(a,b){
+    return a - b;
+}
+
+module.exports = {
+    add,
+    subtract
+};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,13 @@
-const add = require('./index')
+const { add, subtract } = require('./index')
 
 test('Menambahkan 1 + 2 sama dengan 3',()=>{
     expect(add(1,2)).toBe(3)
+});
+
+test('Mengurangi 5 - 3 sama dengan 2',()=>{
+    expect(subtract(5,3)).toBe(2)
+});
+
+test('Mengurangi 10 - 7 sama dengan 3',()=>{
+    expect(subtract(10,7)).toBe(3)
 });
